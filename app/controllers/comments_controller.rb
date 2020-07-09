1502c4e6792ec09f31fa09ef2a2cd477f0670174
N-Santiago <norbertosantiago82@gmail.com>
2020-07-08 16:04:32 -0800

    updates to comments controller and routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,24 @@
 class CommentsController < ApplicationController
-    before_action :set_review, only: [:create, :destroy]
-    before_action :set_comments, only: [:destroy]
+    before_action :set_review, only: [:show, :new, :create, :edit, :update, :destroy]
+    before_action :set_comments, only: [:show, :edit, :update, :destroy]
+
+    def show
+    end 
+
+    def new
+    end 
 
     def create
         @comment = @review.comments.create(comment_params)
         redirect_to post_path(@post)    
     end
+
+    def edit
+    end 
+
+    def update
+        @comment = @review.comments.create(comment_params)
+    end 
   
     def destroy
         @comment.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/genres', to: 'categories#index'
   get '/genres/:id', to: 'categories#show'
   resources :reviews
+  resources :comments, only: [:show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
