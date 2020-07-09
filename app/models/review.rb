ce54c7050a571376b4d678ef2d6691ac60fb89a5
N-Santiago <norbertosantiago82@gmail.com>
2020-07-09 12:57:20 -0800

    reviews index in created_at descending order

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,5 +8,5 @@ class Review < ApplicationRecord
     has_many :users, through: :comments
     validates :title, presence: true, length: { in: 3..30 }
     validates :content, presence: true, length: { maximum: 250 }
-    
+    default_scope { order(created_at: :desc)}
 end
