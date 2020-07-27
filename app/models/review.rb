64054e96b14a47f682017cfa20f3ed4ef3d730f1
N-Santiago <norbertosantiago82@gmail.com>
2020-07-27 13:27:43 -0800

    increased the title length

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,7 +6,7 @@ class Review < ApplicationRecord
     # dependent: :destroy means the comments related 
     # to the specific review in mention get deleted if the post does.
     has_many :users, through: :comments
-    validates :title, presence: true, length: { in: 3..30 }
+    validates :title, presence: true, length: { in: 3..35 }
     validates :content, presence: true, length: { maximum: 250 }
     default_scope { order(created_at: :desc)}
     #by default, the reviews will be displayed from newest to oldest.
