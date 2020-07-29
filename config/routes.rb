3c88b0731e1716988e3fc208acd9f498537e3c61
N-Santiago <norbertosantiago82@gmail.com>
2020-07-29 14:41:27 -0800

    users show and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :users, only: [:show, :index]
   root 'welcome#home'
   get '/genres', to: 'categories#index'
   get '/genres/:id', to: 'categories#show'
+  get 'users/:id/genres/:id', to: 'users#category'
   resources :reviews
   resources :comments, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :reviews do
