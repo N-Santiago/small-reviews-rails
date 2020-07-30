f895405fd1f2936ac8e64ccee222fa34b2ca750e
N-Santiago <norbertosantiago82@gmail.com>
2020-07-29 16:23:05 -0800

    nested categories and back link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,9 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def category
+    @category = Category.find(params[:id])
+    render template: 'users/genres'
+  end
+
 end
