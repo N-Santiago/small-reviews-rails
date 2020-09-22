cae83d9dfa99a9bd30e0ddaf741f69c2a2e34844
N-Santiago <norbertosantiago82@gmail.com>
2020-09-22 12:09:23 -0500

    debugged devise and webpacker issues

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
-
-environment.plugin.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    JQuery: 'jquery',
-    Popper: ['popper.js', 'default']
-}))
 
 module.exports = environment
