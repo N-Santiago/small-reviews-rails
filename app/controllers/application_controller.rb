f840fa2672ba6839df22e7a78888072583621dac
N-Santiago <norbertosantiago82@gmail.com>
2022-02-15 21:07:30 -0600

    Fixed Pundit Authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    include Pundit
+    include Pundit::Authorization
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     protect_from_forgery with: :exception
     before_action :authenticate_user!, except: [:home, :index, :show] 
